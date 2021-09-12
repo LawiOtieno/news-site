8badc9e5c9598f8ea850bcc3550a8a709adeb60f
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-09-12 16:56:22 +0300

    get new function - request

--- app/request.py
+++ app/request.py
@@ -16,3 +16,19 @@ def configure_request(app):
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_API_BASE_URL']
     articles_base_url = app.config['ARTICLES_BASE_URL']
+
+
+
+def get_news():
+    """
+    Function that gets the json response to our url request
+    """
+    get_news_url ='https://newsapi.org/v2/top-headlines/sources?apiKey={NEWS_API_KEY}'
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+        news_results = None
+        if get_news_response['sources']:
+            news_results_list = get_news_response['sources']
+            news_results = process_results(news_results_list)
+    return news_results
